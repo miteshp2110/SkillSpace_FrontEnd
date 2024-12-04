95e4de7193331bf6c563c15041bd3dcc9897224a
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-12-05 01:43:10 +0530

    Added all endpoints

--- src/components/teacher/TeacherRouter.js
+++ src/components/teacher/TeacherRouter.js
@@ -3,16 +3,24 @@ import Home from "./Home/Home";
 import Navbar from "./Navbar/Navbar";
 import Footer from "../common/Footer/Footer";
 import ErrorPage from "../common/ErrorPage/ErrorPage";
+import EmailVerification from "../admin/EmailVerification/EmailVerification";
+import AboutUs from "../common/AboutUs/AboutUs";
+import ProfileCompletion from "./ProfileCompletion/ProfileCompletion";
+import Profile from "./Profile/Profile";
 
 function TeacherRouter() {
     return (
         <>
-            <Navbar/>
+
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/emailVerification" element={<EmailVerification />} />
+                <Route path="/profileCompletion" element={<ProfileCompletion />} />
+                <Route path="/profile" element={<Profile />}  />
 
-
+                <Route  path="/about" element={<AboutUs/>}  />
                 <Route path="*" element={<ErrorPage/>} />
             </Routes>
             <Footer/>
--- src/utils/controllers/AuthControllers.js
+++ src/utils/controllers/AuthControllers.js
@@ -3,29 +3,11 @@ import {getRequest, postRequest} from "../RequestMaker";
 const host = process.env.REACT_APP_AUTH_SERVICE_HOST;
 const port = process.env.REACT_APP_AUTH_SERVICE_PORT;
 
-const sampleToken = "eyJhbGciOiJIUzM4NCJ9.eyJyb2xlIjoiYWRtaW4iLCJzdWIiOiJwYWxpd2FsbWl0ZXNoMjExMEBnbWFpbC5jb20iLCJpYXQiOjE3MzIxNzIwNjIsImV4cCI6MTczMjI1ODQ2Mn0.kEaM5LP6O3wYY4j-J0QPj0cdgiEgCOpronSUBWiG_hkNGBK3W_cqU5uXx8_3iUI2"
-const sampleTokenInvalid = "eyJhbGciOiJIUzM4NCJ9.eyJyb2xlIjoiYWRtaW4iLCJzdWIiOiJwYWxpd2FsbWl0ZXNoMjExMEBnbWFpbC5jb20iLCJpYXQiOsdasdaeawdawasdawdjI1ODQ2Mn0.kEaM5LP6O3wYY4j-J0QPj0cdgiEgCOpronSUBWiG_hkNGBK3W_cqU5uXx8_3iUI2"
-const sampleTokenExpired = "eyJhbGciOiJIUzM4NCJ9.eyJyb2xlIjoiYWRtaW4iLCJzdWIiOiJwYWxpd2FsbWl0ZXNoMjExMEBnbWFpbC5jb20iLCJpYXQiOjE3MzA4OTAxNzgsImV4cCI6MTczMDk3NjU3OH0.vrpx_Gn8AUkuTzlQbHYzke_BmhEAPTPm9PNI7OIlgI0f_pmYR-dtU3YbPZrYY_oQ"
-
 export async function getDepartments(){
     const url = `http://${host}:${port}/departments`;
     return await getRequest(url);
 }
 
-export async function jwtStatus(){
-    const url = `http://${host}:${port}/jwtStatus`;
-    let header = {
-        Authorization:"Bearer "+sampleTokenExpired
-    }
-    return await postRequest(url,null,header);
-}
-export async function getRefreshToken(){
-    const url = `http://${host}:${port}/refreshToken`;
-    let header = {
-        Authorization:"Bearer "+sampleTokenInvalid
-    }
-    return await postRequest(url,null,header);
-}
 export async function login(email,password){
     const url = `http://${host}:${port}/login`;
     let body ={
@@ -51,6 +33,14 @@ export async function completeStudentProfile(formData,token){
     }
     return await postRequest(url,formData,header)
 
+}
+
+export async function completeTeacherProfile(formData,token){
+    const url = `http://${host}:${port}/completeProfile/teacher`;
+    let header = {
+        Authorization:"Bearer "+token
+    }
+    return await postRequest(url,formData,header)
 }
 
 
