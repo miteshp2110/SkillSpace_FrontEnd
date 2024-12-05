08582c1755f4f0bf15a41cf876daf7f85708bc3d
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-12-05 23:46:50 +0530

    Added loading screens

---
+++ src/components/common/LoadingComponent/SingleLineBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SingleLineBar.css";
+
+const SingleLineBar = () => {
+    return (
+        <div className="loading-bar-container">
+            <div className="moving-line"></div>
+        </div>
+    );
+};
+
+export default SingleLineBar;
